452d03dbaea82523de004fc7b914556bb263fc5d
«Vova <vrobskiy@gmail.com>
2022-04-14 22:42:23 +0300

    add exapmle 005  Please enter the commit message for your changes. Lines starting

---
+++ 005/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите первое число: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+Proverka(numberA, numberB);
+
+
+void Proverka(double numberA, double numberB)
+{
+
+if (numberB < numberA)
+{
+    Console.WriteLine("Ошибка! Второе число меньше первого!");
+}
+else
+{
+    double result = numberB % numberA;
+if (result == 0)
+{
+    Console.WriteLine("Число " + numberB + " кратно числу " + numberA);
+}
+else 
+{
+    Console.WriteLine("Остаток от деление равно: " + result);
+} 
+}
+}
