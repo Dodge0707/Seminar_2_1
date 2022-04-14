187373f1ad100308458ec7647e04ff2ff2f2332a
«Vova <vrobskiy@gmail.com>
2022-04-14 20:18:36 +0300

    add seminar 2_1_1

---
+++ 001/Program.cs
@@ -0,0 +1,21 @@
+﻿int sum = Sum(15, 62);
+Print(Convert.ToString(sum));
+int[] numbers = {5, 7, 8};
+int[] numbers2 = new int [5];
+int[] numbers3 = {};
+int[] numbers4;
+number[0] = 10;
+
+int number = 0; // пустое инт
+string str = ""; // пустой стринг - строка
+
+int Sum(int numberOne, int numberTwo)
+{
+    int sum = numberOne + numberTwo;
+    return sum;
+}
+
+void Print(string argument)
+{
+    Console.WriteLine(argument);
+}
