7a120cf1bb8e0092e0c33057b52fb2d957cbe370
«Vova <vrobskiy@gmail.com>
2022-04-14 21:23:48 +0300

    add Delete number 2

---
+++ 004_Task/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = RandomInt(100, 999);
+int numberA = number / 100;
+int numberB = number % 10;
+Console.WriteLine(number);
+//Console.WriteLine(numberA);
+//Console.WriteLine(numberB);
+Console.WriteLine(Convert.ToString(numberA) + Convert.ToString(numberB));
+
+
+int RandomInt(int min, int max)
+ {
+     return new Random().Next(min, max);
+ }
