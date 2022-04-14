9fe4e1dabba03512a6c3389d182348346d8983b6
«Vova <vrobskiy@gmail.com>
2022-04-14 21:05:33 +0300

    add functions

---
+++ 003 Example_int/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = new Random().Next(10, 100);
+int first = number / 10;
+int second = number % 10;
+Console.WriteLine(number);
+
+int max = MaxNumber(first, second);
+Console.WriteLine(max);
+
+ int RandomInt(int min, int max)
+ {
+     return new Random().Next(min, max);
+ }
+
+ 
+int MaxNumber(int number1, int number2)
+{
+   
+    if(number1 > number2)
+        return number1;
+    
+    return number2;
+}
